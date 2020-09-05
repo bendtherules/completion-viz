6024509516f37bcf9c131cf896081f45d1947485
bendtherules <abhasbhattacharya2@gmail.com>
2020-09-05 17:25:45 +0530

    Add basic code view and sourceMapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+
+import { sourceMapping } from "./sourceMapping";
+
+import "./tailwind.output.css";
+
+import "./App.css";
+
+const exampleCode = `var test = \`asd
+World!\`; abc()`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Highlight {...defaultProps} code={exampleCode} language="jsx">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          sourceMapping.reset(),
+          (
+            <pre className={className} style={style}>
+              {tokens.map((line, i) => (
+                <div
+                  data-line={JSON.stringify(line)}
+                  {...getLineProps({ line, key: i })}
+                >
+                  {line.map(function (token, key) {
+                    const refFn = sourceMapping.registerToken(token);
+                    return (
+                      <span
+                        ref={refFn}
+                        data-token={JSON.stringify(token)}
+                        {...getTokenProps({ token, key })}
+                      />
+                    );
+                  })}
+                  {(sourceMapping.registerLineEnd(), null)}
+                </div>
+              ))}
+            </pre>
+          )
+        )}
+      </Highlight>
     </div>
   );
 }
---
+++ src/sourceMapping.tsx
@@ -0,0 +1,50 @@
+import { Token } from "./prism-types";
+
+interface TMappingELement {
+  start: number;
+  end: number;
+  ref: { current: HTMLElement | null };
+}
+
+interface TSourceMapping {
+  mappingArray: Array<TMappingELement>;
+  lastEnd: number;
+  registerToken(t: Token): (ele: HTMLElement | null) => void;
+  registerLineEnd: () => void;
+  reset: () => void;
+  getElementsInRange(start: number, end: number): HTMLElement[];
+}
+
+export const sourceMapping: TSourceMapping = {
+  mappingArray: [],
+  lastEnd: 0,
+  registerToken(t: Token) {
+    const obj: TMappingELement = {
+      start: this.lastEnd,
+      end: this.lastEnd + t.content.length,
+      ref: { current: null },
+    };
+    this.mappingArray.push(obj);
+    this.lastEnd = this.mappingArray[this.mappingArray.length - 1].end;
+
+    console.log(obj, t);
+    
+
+    return function updateHTMLElement(ele) {
+      obj.ref.current = ele;
+    };
+  },
+  registerLineEnd() {
+    this.lastEnd += 1;
+    if (this.mappingArray.length > 0) {
+      this.mappingArray[this.mappingArray.length - 1].end = this.lastEnd;
+    }
+  },
+  reset() {
+    this.mappingArray = [];
+    this.lastEnd = 0;
+  },
+  getElementsInRange(start, end) {
+    return [];
+  },
+};
