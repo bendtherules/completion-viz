8a495eae12f8b17bbe4e647f055a77642e309c0d
bendtherules <abhasbhattacharya2@gmail.com>
2020-09-07 18:53:23 +0530

    Delete engine262 project

--- engine262/src/intrinsics/AsyncFunctionPrototype.mjs
+++
@@ -1,7 +0,0 @@
-import { BootstrapPrototype } from './Bootstrap.mjs';
-
-export function BootstrapAsyncFunctionPrototype(realmRec) {
-  const proto = BootstrapPrototype(realmRec, [], realmRec.Intrinsics['%Function.prototype%'], 'AsyncFunction');
-
-  realmRec.Intrinsics['%AsyncFunction.prototype%'] = proto;
-}
--- engine262/src/runtime-semantics/AsyncArrowFunction.mjs
+++
@@ -1,7 +0,0 @@
-import { Value } from '../value.mjs';
-import { NamedEvaluation } from './all.mjs';
-
-export function Evaluate_AsyncArrowFunction(AsyncArrowFunction) {
-  // 1. Return the result of performing NamedEvaluation for this ArrowFunction with argument "".
-  return NamedEvaluation(AsyncArrowFunction, new Value(''));
-}
