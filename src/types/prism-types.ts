7d4b2f15a3b9660e2840e2ce055153301cbeb2c8
bendtherules <abhasbhattacharya2@gmail.com>
2020-10-05 17:25:32 +0530

    update(types): Expose more types from prism

--- src/types/prism-types.ts
+++ src/types/prism-types.ts
@@ -3,3 +3,49 @@ export type Token = {
   content: string;
   empty?: boolean;
 };
+
+export type RenderProps = {
+  tokens: Token[][];
+  className: string;
+  style: StyleObj;
+  getLineProps: (input: LineInputProps) => LineOutputProps;
+  getTokenProps: (input: TokenInputProps) => TokenOutputProps;
+};
+
+export type StyleObj = {
+  [key: string]: string | number | null;
+};
+
+export type LineInputProps = {
+  key?: React.Key;
+  style?: StyleObj;
+  className?: string;
+  line: Token[];
+  [otherProp: string]: any;
+};
+
+export type LineOutputProps = {
+  key?: React.Key;
+  style?: StyleObj;
+  className: string;
+  [otherProps: string]: any;
+};
+
+export type TokenInputProps = {
+  key?: React.Key;
+  style?: StyleObj;
+  className?: string;
+  token: Token;
+  [otherProp: string]: any;
+};
+
+export type TokenOutputProps = {
+  key?: React.Key;
+  style?: StyleObj;
+  className: string;
+  children: string;
+  [otherProp: string]: any;
+};
+
+export type TgetLineProps = (input: LineInputProps) => LineOutputProps;
+export type TgetTokenProps = (input: TokenInputProps) => TokenOutputProps;
